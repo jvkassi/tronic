e52353b606600d68d6645eb6199970e401ea060b
Richard Solomou <richard@richardsolomou.com>
2018-06-13 15:58:49 +0100

    6

--- src/config/local-config.js
+++ src/config/local-config.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-empty, import/no-mutable-exports */
 import { resolveFromCwd } from '../utils'
 
-let localConfig = {}
-try {
-  localConfig = require(`${resolveFromCwd('webpack.config.js')}`)
-} catch (e) {
+async function getLocalConfig() {
+  const config = await import(`${resolveFromCwd('webpack.config.js')}`)
+  const babelConfig = await import(`${resolveFromCwd('webpack.config.babel.js')}`)
+
+  return config || babelConfig || {}
 }
 
-export default localConfig
+export default getLocalConfig()

65cb03275e1c17157a21ba4a78ae626e9a0a9bd5
Richard Solomou <richard@richardsolomou.com>
2018-06-14 15:39:43 +0100

    removed install-module

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 export { default as importIfExists } from './import-if-exists'
-export { default as installModule } from './install-module'
 export { default as output } from './output'
 export { default as resolveFromCwd } from './resolve-from-cwd'

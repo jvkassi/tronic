cfddd6c01d176c6470f9335a4342f71dae4cb989
Richard Solomou <richard@richardsolomou.com>
2018-06-14 16:02:08 +0100

    bin

--- src/bin/start.js
+++ src/bin/start.js
@@ -1,6 +1,6 @@
 import webpack from 'webpack'
-import config from './webpack/config'
-import output from './utils/output'
+import config from '../webpack/config'
+import output from '../utils/output'
 
 export default function start() {
   const compiler = webpack(config)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import dotenv from 'dotenv'
 import yargs from 'yargs'
-import build from './build'
-import lint from './lint'
-import start from './start'
+import { build, start } from './bin'
 
 dotenv.config()
 
 yargs
   .command(['$0', 'start'], 'Start bundle', start)
   .command('build', 'Run bundle', build)
-  .command('lint', 'Lint', lint)
   .parse()
 

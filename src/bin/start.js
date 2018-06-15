01924c040500022b5c136cce671355d1e095dfd8
Richard Solomou <richard@richardsolomou.com>
2018-06-15 12:37:50 +0100

    serve

--- src/bin/start.js
+++ src/bin/start.js
@@ -1,13 +1,6 @@
-import webpack from 'webpack'
-import WebpackDevServer from 'webpack-dev-server'
+import serve from 'webpack-serve'
 import { config } from '../webpack'
 
 export default function start() {
-  const compiler = webpack(config)
-  const server = new WebpackDevServer(compiler, config.devServer)
-  server.listen(config.devServer.port, '127.0.0.1', () => {
-    console.log()
-    console.log(`Starting server on http://localhost:${config.devServer.port}`)
-    console.log()
-  })
+  return serve({ config })
 }

7571a384302bf05e7c9cf3c45dd37b934b26c6d3
Richard Solomou <richard@richardsolomou.com>
2018-06-15 13:05:26 +0100

    Revert "serve"
    This reverts commit 01924c040500022b5c136cce671355d1e095dfd8.

--- src/bin/start.js
+++ src/bin/start.js
@@ -1,6 +1,13 @@
-import serve from 'webpack-serve'
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
 import { config } from '../webpack'
 
 export default function start() {
-  serve({ config })
+  const compiler = webpack(config)
+  const server = new WebpackDevServer(compiler, config.devServer)
+  server.listen(config.devServer.port, '127.0.0.1', () => {
+    console.log()
+    console.log(`Starting server on http://localhost:${config.devServer.port}`)
+    console.log()
+  })
 }

237a403807e3b90652fbdacc4009795e0ff15b00
Richard Solomou <richard@richardsolomou.com>
2018-06-16 03:06:28 +0100

    Added examples

---
+++ examples/babel/src/index.js
@@ -0,0 +1,9 @@
+import cowsay from 'cowsay'
+
+console.log(
+  cowsay.say({
+    text: "I'm a moooodule",
+    e: 'oO',
+    T: 'U ',
+  }),
+)
---
+++ examples/json/src/index.js
@@ -0,0 +1,3 @@
+const json = require('./data.json')
+
+module.exports = json

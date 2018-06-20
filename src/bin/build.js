ca09b28f875f0bc80e278154dc05be48853158f1
Richard Solomou <richard@richardsolomou.com>
2018-06-20 16:40:16 +0100

    refactoring

--- src/bin/build.js
+++ src/bin/build.js
@@ -6,40 +6,52 @@ import { config } from '../webpack'
 // Set flags
 yargs.option('watch', { alias: 'w', default: false })
 
+let spinner
+
+/**
+ * Start spinner on initial run
+ */
+function handleRun() {
+  spinner = ora('Bundling').start()
+}
+
+/**
+ * Output compilation progress and errors
+ * @param {Object} stats
+ */
+function handleDone(stats) {
+  if (stats.hasErrors()) {
+    spinner.fail('Failed to bundle')
+  } else {
+    spinner.succeed('Bundled')
+  }
+
+  const info = stats.toString(config.stats)
+  if (info) {
+    console.log(info)
+    console.log()
+  }
+}
+
+/**
+ * Output failure error
+ * @param {string} err
+ */
+function handleFailed(err) {
+  if (err) {
+    console.log(err)
+  }
+}
+
 /**
  * Attach hooks to compiler
  * @param {Compiler} compiler
  */
 function hook(compiler) {
-  let spinner
-
-  const startSpinner = () => {
-    spinner = ora('Bundling').start()
-  }
-
-  compiler.hooks.watchRun.tap('tronic', startSpinner)
-  compiler.hooks.run.tap('tronic', startSpinner)
-
-  compiler.hooks.done.tap('tronic', (stats) => {
-    if (stats.hasErrors()) {
-      spinner.fail('Failed to bundle')
-    } else {
-      spinner.succeed('Bundled')
-    }
-
-    const info = stats.toString(config.stats)
-    if (info) {
-      console.log(info)
-      console.log()
-    }
-  })
-
-  compiler.hooks.failed.tap('tronic', (err) => {
-    if (err) {
-      console.log(err)
-    }
-  })
-
+  compiler.hooks.watchRun.tap('tronic', handleRun)
+  compiler.hooks.run.tap('tronic', handleRun)
+  compiler.hooks.done.tap('tronic', handleDone)
+  compiler.hooks.failed.tap('tronic', handleFailed)
   return compiler
 }
 

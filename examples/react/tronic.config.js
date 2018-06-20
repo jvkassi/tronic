d86a38a524e559b0a50a7b22b91f8f6c375ba4e4
Richard Solomou <richard@richardsolomou.com>
2018-06-20 16:28:50 +0100

    hooks

--- examples/react/tronic.config.js
+++ examples/react/tronic.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     'babel',
+    'eslint',
     'html',
   ],
 }
--- src/bin/build.js
+++ src/bin/build.js
@@ -7,41 +7,51 @@ import { config } from '../webpack'
 yargs.option('watch', { alias: 'w', default: false })
 
 /**
- * Display compilation output
- * @param {Object} spinner
- * @param {string} err
- * @param {Object} stats
+ * Attach hooks to compiler
+ * @param {Compiler} compiler
  */
-let callback = (spinner, err, stats) => {
-  if (err) {
-    spinner.fail('Failed to bundle')
-    console.log(err)
-    return
+function hook(compiler) {
+  let spinner
+
+  const startSpinner = () => {
+    spinner = ora('Bundling').start()
   }
 
-  if (stats.hasErrors()) {
-    spinner.fail('Failed to bundle')
-  } else {
-    spinner.succeed('Bundled')
-  }
+  compiler.hooks.watchRun.tap('tronic', startSpinner)
+  compiler.hooks.run.tap('tronic', startSpinner)
 
-  const info = stats.toString(config.stats)
-  if (info) {
-    console.log(info)
-  }
+  compiler.hooks.done.tap('tronic', (stats) => {
+    if (stats.hasErrors()) {
+      spinner.fail('Failed to bundle')
+    } else {
+      spinner.succeed('Bundled')
+    }
+
+    const info = stats.toString(config.stats)
+    if (info) {
+      console.log(info)
+      console.log()
+    }
+  })
+
+  compiler.hooks.failed.tap('tronic', (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+
+  return compiler
 }
 
 /**
  * Build bundle to disk
  */
 export default function build() {
-  const spinner = ora('Bundling').start()
-  callback = callback.bind(null, spinner)
-  const compiler = webpack(config)
+  const compiler = hook(webpack(config))
   // Run watcher
   if (yargs.argv.watch) {
-    return compiler.watch({}, callback)
+    return compiler.watch({}, () => {})
   }
   // Run one-off build
-  return compiler.run(callback)
+  return compiler.run()
 }
